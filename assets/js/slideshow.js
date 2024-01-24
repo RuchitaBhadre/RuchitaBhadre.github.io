b42a6135f107ecbc758973ba70c3ef4776754882
Ruchita Rajkumar Bhadre <55748211+RuchitaBhadre@users.noreply.github.com>
2024-01-24 12:03:24 -0500

    Update slideshow.js

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,21 +1,31 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// Function to control slides for a specific slideshow
+function plusSlides(n, slideshowId) {
+  showSlides(slideIndex[slideshowId] += n, slideshowId);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+// Function to display slides for a specific slideshow
+function showSlides(n, slideshowId) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) { slideIndex = 1; }
-  if (n < 1) { slideIndex = slides.length; }
+  var slides = document.querySelectorAll(`#${slideshowId} .mySlides`);
+  if (n > slides.length) { slideIndex[slideshowId] = 1; }
+  if (n < 1) { slideIndex[slideshowId] = slides.length; }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex[slideshowId] - 1].style.display = "block";
 }
+
+// Initialize slide index for each slideshow
+var slideIndex = {
+  'slideshow1': 1,
+  'slideshow2': 1,
+  'slideshow3':1,
+  // Add more slide indexes for additional slideshows
+};
+
+// Show the initial slides for each slideshow
+showSlides(slideIndex['slideshow1'], 'slideshow1');
+showSlides(slideIndex['slideshow2'], 'slideshow2');
+showSlides(slideIndex['slideshow3'], 'slideshow3');
+
+
